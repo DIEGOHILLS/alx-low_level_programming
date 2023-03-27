bdf3cd619b1aba9c3a000ba8ee70c43cfb78875a
Your Name <you@example.com>
2023-03-27 15:01:22 -0700

    advanced

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,14 @@
 #include "main.h"
-#include <stdio>
+
 /**
  * _puts - prints a string, followed by a new line, to stdout
  * @str: string to print
  */
+
 void _puts(char *str)
+
 {
-	while (*str != '\0')
+	for (;*str != '\0'; str++)
 	{
 		_putchar(*str++);
 	}
